4432a4b48264304a7ec291591d26053303eac915
Galzria <jbgasnier@inspire-tse.com>
2012-03-30 10:35:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from operators import add, nul
+from logic import and_,or_
+
+def main:
+    print "my application starts..."
+    a=2
+    b=3
+    x=True
+    y=False
+    print add(a,b) 
+    print nul (x,y)
+    print and_(x,y)
+     print or_(x,y)
+
+if __name__=="__main__"
+    main()
